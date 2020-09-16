2572b18c3e31360a34bb48ba9efa3c84b8165d8c
galojose38 <galojose38@gmail.com>
2020-09-15 19:44:52 -0500

    cambio

--- vistas/modulos/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-	
-	<strong>Copyright &copy; 2019 <a href="https://www.compartiendocodigos.net/" target="_blank">Compartiendo Códigos</a>.</strong>
-
-	Todos los derechos reservados.
-
-
-</footer>
